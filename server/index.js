41c8a896ca5c46e6459b610360e47ba897a00cb0
Icarus-afk <ahmed.ehsan1258@gmail.com>
2024-01-03 21:01:02 +0600

    dummy backend code

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose  from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+const app = express(); //initialize express instance
+
+app.use('/posts', postRoutes)
+
+// setting up body parser
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+
+//setting up cors
+app.use(cors());
+const  MONGO_STRING = "mongodb://127.0.0.1:27017/SM?&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1"
+const PORT = process.env.PORT || 8000;
+
+//connecting to Mongodb
+mongoose.connect(String(MONGO_STRING))
+    .then(() => app.listen(PORT, () => console.log(`Server running on port = ${PORT}`)))
+    .catch((error) => console.log (`Error Occured ---> ${error.message}`))
+
